4c68c0935045af076bdd63cdb952e4cd02f1258e
JONATHAN GARCIA SANTOS <158236706+JONATHAN-GARCIA20@users.noreply.github.com>
2024-11-12 13:46:45 -0800

    Create Ejercicio32.s

--- Programas del 31 al 40/Ejercicio32.s
+++ Programas del 31 al 40/Ejercicio32.s
@@ -12,6 +12,9 @@
  ║                           Septiembre 2024                          ║
  ╚════════════════════════════════════════════════════════════════════╝
 
+//Video
+https://asciinema.org/a/PuZbbA9QL2L8zdGlmbZumsv77
+
 // Programa en C#
 
 using System;
